f8f1da7fabeccbb635bb18f09d6715d8c52eff17
Karl-Johan Westhoff <karl.johan.westhoff@gmail.com>
2020-11-10 10:24:14 -0800

    page-builder with require

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
 
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-
-const printProfileData = profileDataArr => {
-    // Jadda Jadda
-    for(let i = 0; i < profileDataArr.length; i+=1){
-        console.log(profileDataArr[i]);
-    };
-
-    console.log("=============================");
-
-    profileDataArr.forEach((profileItem) => {
-        // Same as jadda jadda
-        console.log(profileItem)
-        
-    });
-}; 
+const [name,github] = profileDataArgs;
 
 
-printProfileData(profileDataArgs);
+  
+fs.writeFile('index.html', generatePage(name, github),err => {
+    if(err) throw err;
+
+    console.log("Portfolit comeplete - see index.html")
+
+})
 
 
